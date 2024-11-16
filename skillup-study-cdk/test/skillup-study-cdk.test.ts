86d3c4b73e13ba2f3b783f49faa25c9e42d35a44
mpierrot <kurntogasmile@gmail.com>
2024-11-16 16:01:05 +0900

    first development create

---
+++ skillup-study-cdk/test/skillup-study-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SkillupStudyCdk from '../lib/skillup-study-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/skillup-study-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SkillupStudyCdk.SkillupStudyCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
